fffaa66bf3ae641aa31c284bb8c8f08bcd5021f1
ebremner <ebremner@uwaterloo.ca>
2020-11-13 00:41:13 -0500

    Fixing board of directors stuff and js for conference menu

--- js/conference_menu.js
+++ js/conference_menu.js
@@ -1,20 +1,23 @@
 (function ($, Drupal) {
   Drupal.behaviors.conference_menu = {
     attach: function (context, settings) {
-      $(document, context).once('conference_menu').each( function() {
 
-        var acc = document.getElementsByClassName("cuba-accordion");
-        var i;
+      $(document).ready(function(){
 
-        for (i = 0; i < acc.length; i++) {
-          acc[i].addEventListener("click", function () {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
-            this.classList.toggle("active");
+        $(document, context).once('conference_menu').each( function() {
 
+          acc = document.getElementsByClassName("cuba-accordion");
 
-          });
-        }
+          for (i = 0; i < acc.length; i++) {
+
+            acc[i].addEventListener("click", function (acc_clicked) {
+
+              /* Toggle between adding and removing the "active" class,
+              to highlight the button that controls the panel */
+              this.classList.toggle("active");
+            });
+          }
+        });
       });
     }
   };
